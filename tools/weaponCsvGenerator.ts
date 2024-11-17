5874644550c4c97894d27caa99567168cf27255d
suiwave <184898096+suiwave@users.noreply.github.com>
2024-11-17 15:15:40 +0900

    ちょっと進捗

--- tools/weaponCsvGenerator.ts
+++ tools/weaponCsvGenerator.ts
@@ -9,7 +9,10 @@ const WIKI_URL = 'https://kamikouryaku.net/eldenring/?%E7%9F%AD%E5%89%A3';
 // パースして作成するWeaponオブジェクト
 interface Weapon {
     name: string;
+    upgrade_path: string;
     arts: string;
+    getMethod: string;
+    effect: string;
 }
 
 const fetchWeaponData = async (url: string) => {
@@ -44,12 +47,59 @@ const fetchWeaponData = async (url: string) => {
 
 const convertObject = (weaponNameElm: cheerio.Cheerio, elwElm: cheerio.Cheerio): Weapon => {
     const name = weaponNameElm.find('.link_page_passage').text()
-    const arts = elwElm
-        .find('.flexbox > div:nth-child(2) .ie5 table tbody tr:first-child td a.link_page_passage')
-        .text();
+
+    const elwTable = elwElm.children()
+    /**
+     * こんな感じのdivが4つ並んでいるので、4つを取得する
+    攻撃力/カット率
+    ---------------------------------------------------------
+    | 物 | 魔 | 炎 | 雷 | 聖 | 致/ガ | 補/射 | 強化 |
+    ---------------------------------------------------------
+    | 81  | 0  | 0  | 0  | 0  | 110   | -    | 通常 |
+    | 36.0 | 21.0 | 21.0 | 21.0 | 21.0 | 15-18 | -    | - |
+    ---------------------------------------------------------
+
+    能力補正/必要能力
+    ---------------------------------------------------------
+    | 筋 | 技 | 知 | 信 | 神 | 属性  | 戦技/消費FP/戦灰 | 重量 |
+    ---------------------------------------------------------
+    | C  | E  | -  | -  | -  | 斬撃・刺突 | クイックステップ | 2.0 |
+    | 9  | 12 | 0  | 0  | 0  | 3(- /- )   | ◯  |
+    ---------------------------------------------------------
+
+    入手方法
+    ---------------------------------------------------------
+    亜人の親分がドロップ
+    ---------------------------------------------------------
+
+    付帯効果
+    ---------------------------------------------------------
+    出血の状態異常を蓄積する(38)
+    ---------------------------------------------------------
+     */
+    // 攻撃力/カット率/致命倍率/ガード強度/強化方法
+    const atackTable = elwTable.children('div').eq(0)
+
+    // 能力補正/必要能力/属性/戦技/消費FP/戦灰/重量
+    const specTable = elwTable.children('div').eq(1)
+
+    // 入手方法
+    const getMethodTable = elwTable.children('div').eq(2)
+
+    // 付帯効果
+    const effectTable = elwTable.children('div').eq(3)
+
+    const upgrade_path = atackTable.find('.ie5 table tbody tr:first-child td:last-child').text();
+    const arts = specTable.find('.ie5 table tbody tr:first-child td a.link_page_passage').text();
+    const getMethod = getMethodTable.find('td').text().replace('入手方法', '')
+    const effect = effectTable.find('td').text().replace('付帯効果', '')
+    // TODO：その他項目も取得する
     return {
         name,
+        upgrade_path,
         arts,
+        getMethod,
+        effect,
     }
 }
 
