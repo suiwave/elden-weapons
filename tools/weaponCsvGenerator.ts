707896b949e113f66e094f1f2d47e74960fd8e36
suiwave <184898096+suiwave@users.noreply.github.com>
2024-11-23 20:10:53 +0900

    json作成までは完了？urlも記載

--- tools/weaponCsvGenerator.ts
+++ tools/weaponCsvGenerator.ts
@@ -4,15 +4,70 @@ import * as cheerio from 'cheerio';
 // import * as path from 'path';
 
 // WikiページのURL（例）
-const WIKI_URL = 'https://kamikouryaku.net/eldenring/?%E7%9F%AD%E5%89%A3';
+const WIKI_BASE_URL = 'https://kamikouryaku.net/eldenring';
 
+const WIKI_WEAPON_URLs = [
+    // 近接武器
+    "/?%E7%9F%AD%E5%89%A3",
+    "/?%E6%8A%95%E6%93%B2%E5%89%A3",
+    "/?%E7%9B%B4%E5%89%A3",
+    "/?%E8%BB%BD%E5%A4%A7%E5%89%A3",
+    "/?%E5%A4%A7%E5%89%A3",
+    "/?%E7%89%B9%E5%A4%A7%E5%89%A3",
+    "/?%E5%88%BA%E5%89%A3",
+    "/?%E9%87%8D%E5%88%BA%E5%89%A3",
+    "/?%E6%9B%B2%E5%89%A3",
+    "/?%E5%A4%A7%E6%9B%B2%E5%89%A3",
+    "/?%E9%80%86%E6%89%8B%E5%89%A3%28%E6%AD%A6%E5%99%A8%E7%A8%AE%29",
+    "/?%E5%88%80",
+    "/?%E5%A4%A7%E5%88%80",
+    "/?%E4%B8%A1%E5%88%83%E5%89%A3",
+    "/?%E6%96%A7",
+    "/?%E5%A4%A7%E6%96%A7",
+    "/?%E6%A7%8C",
+    "/?%E3%83%95%E3%83%AC%E3%82%A4%E3%83%AB",
+    "/?%E5%A4%A7%E6%A7%8C",
+    "/?%E7%89%B9%E5%A4%A7%E6%AD%A6%E5%99%A8",
+    "/?%E6%A7%8D",
+    "/?%E5%A4%A7%E6%A7%8D",
+    "/?%E6%96%A7%E6%A7%8D",
+    "/?%E9%8E%8C",
+    "/?%E9%9E%AD",
+    "/?%E6%8B%B3",
+    "/?%E6%A0%BC%E9%97%98",
+    "/?%E7%88%AA",
+    "/?%E7%8D%A3%E7%88%AA%28%E6%AD%A6%E5%99%A8%E7%A8%AE%29",
+    "/?%E8%AA%BF%E9%A6%99%E7%93%B6%28%E6%AD%A6%E5%99%A8%E7%A8%AE%29",
+    // 遠距離武器
+    "/?%E5%B0%8F%E5%BC%93",
+    "/?%E9%95%B7%E5%BC%93",
+    "/?%E5%A4%A7%E5%BC%93",
+    "/?%E3%82%AF%E3%83%AD%E3%82%B9%E3%83%9C%E3%82%A6",
+    "/?%E3%83%90%E3%83%AA%E3%82%B9%E3%82%BF",
+    "/?%E7%9F%A2%E3%83%BB%E3%83%9C%E3%83%AB%E3%83%88",
+    "/?%E6%9D%96",
+    "/?%E8%81%96%E5%8D%B0",
+    // 盾
+    "/?%E6%9D%BE%E6%98%8E",
+    "/?%E5%B0%8F%E7%9B%BE",
+    "/?%E4%B8%AD%E7%9B%BE",
+    "/?%E5%A4%A7%E7%9B%BE",
+    "/?%E5%88%BA%E7%AA%81%E7%9B%BE",
+]
 // パースして作成するWeaponオブジェクト
 interface Weapon {
     name: string;
-    upgrade_path: string;
+    wiki_url: string;
     arts: string;
     getMethod: string;
     effect: string;
+    str_required: string;
+    dex_required: string;
+    int_required: string;
+    fth_required: string;
+    myt_required: string;
+    upgrade_path: string;
+    enable_change_arts: string;
 }
 
 const fetchWeaponData = async (url: string) => {
@@ -47,6 +102,7 @@ const fetchWeaponData = async (url: string) => {
 
 const convertObject = (weaponNameElm: cheerio.Cheerio, elwElm: cheerio.Cheerio): Weapon => {
     const name = weaponNameElm.find('.link_page_passage').text()
+    const wiki_url = weaponNameElm.find('.link_page_passage').attr("href") ?? "not found";
 
     const elwTable = elwElm.children()
     /**
@@ -91,20 +147,33 @@ const convertObject = (weaponNameElm: cheerio.Cheerio, elwElm: cheerio.Cheerio):
 
     const upgrade_path = atackTable.find('.ie5 table tbody tr:first-child td:last-child').text();
     const arts = specTable.find('.ie5 table tbody tr:first-child td a.link_page_passage').text();
+    const str_required = specTable.find('.ie5 table tbody tr:last-child td:nth-child(1)').text();
+    const dex_required = specTable.find('.ie5 table tbody tr:last-child td:nth-child(2)').text();
+    const int_required = specTable.find('.ie5 table tbody tr:last-child td:nth-child(3)').text();
+    const fth_required = specTable.find('.ie5 table tbody tr:last-child td:nth-child(4)').text();
+    const myt_required = specTable.find('.ie5 table tbody tr:last-child td:nth-child(5)').text();
+    const enable_change_arts = specTable.find('.ie5 table tbody tr:last-child td:last-child').text();
     const getMethod = getMethodTable.find('td').text().replace('入手方法', '')
     const effect = effectTable.find('td').text().replace('付帯効果', '')
     // TODO：その他項目も取得する
     return {
         name,
-        upgrade_path,
+        wiki_url,
         arts,
         getMethod,
         effect,
+        str_required,
+        dex_required,
+        int_required,
+        fth_required,
+        myt_required,
+        upgrade_path,
+        enable_change_arts,
     }
 }
 
 const main = async () => {
-    fetchWeaponData(WIKI_URL)
+    fetchWeaponData(`${WIKI_BASE_URL}${WIKI_WEAPON_URLs[WIKI_WEAPON_URLs.length - 1]}`)
 }
 
 main()
